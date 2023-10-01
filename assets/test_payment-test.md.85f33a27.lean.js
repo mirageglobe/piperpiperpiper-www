e6921802aeacf4976e78f5a51526cedb2f7f24ba
Jimmy Lim <mirageglobe@gmail.com>
2023-10-01 13:57:04 +0100

    update piper

--- assets/test_payment-test.md.85f33a27.lean.js
+++ assets/test_payment-test.md.85f33a27.lean.js
@@ -1,4 +1,4 @@
-import{C as n,o,c as r,H as i,w as s,Q as t,k as e}from"./chunks/framework.6b68528e.js";const p=t("",6),l=e("div",{id:"paypal-button-container"},null,-1),c=e("pre",null,[e("code",null,`<script src="https://www.paypal.com/sdk/js?client-id=sb&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"><\/script>
+import{C as n,c as o,H as r,w as i,Q as t,o as s,k as e}from"./chunks/framework.a7175731.js";const p=t("",6),l=e("div",{id:"paypal-button-container"},null,-1),c=e("pre",null,[e("code",null,`<script src="https://www.paypal.com/sdk/js?client-id=sb&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"><\/script>
 <script>
   function initPayPalButton() {
     paypal.Buttons({
@@ -39,4 +39,4 @@ import{C as n,o,c as r,H as i,w as s,Q as t,k as e}from"./chunks/framework.6b685
   }
   initPayPalButton();
 <\/script>
-`)],-1),u=t("",3),y=JSON.parse('{"title":"the following paypal snippet is not working - delete","description":"","frontmatter":{"layout":"doc","search":false},"headers":[],"relativePath":"test/payment-test.md","filePath":"test/payment-test.md"}'),d={name:"test/payment-test.md"},g=Object.assign(d,{setup(h){return(m,_)=>{const a=n("ClientOnly");return o(),r("div",null,[p,i(a,null,{default:s(()=>[l,c]),_:1}),u])}}});export{y as __pageData,g as default};
+`)],-1),u=t("",3),y=JSON.parse('{"title":"the following paypal snippet is not working - delete","description":"","frontmatter":{"layout":"doc","search":false},"headers":[],"relativePath":"test/payment-test.md","filePath":"test/payment-test.md"}'),d={name:"test/payment-test.md"},g=Object.assign(d,{setup(h){return(m,_)=>{const a=n("ClientOnly");return s(),o("div",null,[p,r(a,null,{default:i(()=>[l,c]),_:1}),u])}}});export{y as __pageData,g as default};
